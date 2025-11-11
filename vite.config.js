2a7b23c7775dbca5cd8760feb3ba25fe908f9a7b
erosseraph <erosseraph@gmail.com>
2025-11-12 03:40:49 +0800

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  build: {
+    rollupOptions: {
+      input: resolve(__dirname, 'index.html'),
+    },
+  },
+})
